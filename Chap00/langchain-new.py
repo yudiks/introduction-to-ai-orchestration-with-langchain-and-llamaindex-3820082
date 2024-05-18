024ccbbe62798d393e66a45794ba2ae375e23557
yudi sutantom <yudiks@gmail.com>
2024-05-18 01:09:44 -0700

    sequential chain

--- Chap00/langchain-new.py
+++ Chap00/langchain-new.py
@@ -4,13 +4,17 @@
 
 from langchain_openai import OpenAI
 from langchain.prompts import PromptTemplate
-from langchain.chains import LLMChain
 
 prompt_template_name = PromptTemplate(
     input_variables = ['cuisine'],
     template = "U want to open a restaurant for {cuisine} food. Suggest a restaurant name"
 )
-prompt_template_name.format(cuisine="Italian")
+
+prompt_template_items = PromptTemplate(
+    input_variables = ['restaurant_name'],
+    template = "Suggest some menu for {restaurant_name}. Return it as a comma separated list"
+)
+
 
 
 llm = OpenAI(temperature=0.6, 
@@ -18,6 +22,5 @@ llm = OpenAI(temperature=0.6,
     base_url="http://localhost:1234/v1"  # see chapter 1 video 3)
 )
 
-# chain = LLMChain(llm=llm, prompt=prompt_template_name)
-chain = prompt_template_name | llm
+chain = prompt_template_name | prompt_template_items | llm
 print(chain.invoke("Mexican"))
