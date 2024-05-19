408e783a93ec9e00c4cf85ee5638cb717d57c1f7
yudi sutantom <yudiks@gmail.com>
2024-05-19 12:06:10 -0700

    Import CSV

--- Chap05/codebasics_rag.py
+++ Chap05/codebasics_rag.py
@@ -1,6 +1,14 @@
 from langchain_community.document_loaders import TextLoader
+from langchain_community.document_loaders.csv_loader import CSVLoader
 
 loader = TextLoader("Chap05/nvda_news_1.txt")
 data = loader.load()
 print(data[0])
 
+print(data[0].page_content)
+print(data[0].metadata)
+
+
+loader = CSVLoader("Chap05/movies.csv")
+data = loader.load()
+print(data)
