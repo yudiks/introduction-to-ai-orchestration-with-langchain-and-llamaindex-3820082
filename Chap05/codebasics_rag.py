03390a49506329b062ed65d6e3c465090542bc4f
Yudi Sutanto <yudiks@gmail.com>
2024-05-23 22:42:35 -0700

    something about rag

--- Chap05/codebasics_rag.py
+++ Chap05/codebasics_rag.py
@@ -5,10 +5,24 @@ loader = TextLoader("Chap05/nvda_news_1.txt")
 data = loader.load()
 print(data[0])
 
-print(data[0].page_content)
-print(data[0].metadata)
+# # print(data[0].page_content)
+# # print(data[0].metadata)
 
 
-loader = CSVLoader("Chap05/movies.csv")
-data = loader.load()
-print(data)
+# # loader = CSVLoader("Chap05/movies.csv", source_column="title")
+# # data = loader.load()
+# # print(data)
+
+# # pip install unstructured libmagic python-magic python-magic-bin
+# # from langchain.document_loaders import UnstructuredURLLoader
+# from langchain_community.document_loaders import UnstructuredURLLoader
+
+# loader = UnstructuredURLLoader(urls=[
+#     "https://www.moneycontrol.com/europe/?url=https://www.moneycontrol.com/news/technology/nvidia-stock-climbs-as-cfo-says-new-chip-to-ship-in-2024-12487781.html"])
+
+# data = loader.load()
+# len(data)
+
+####################
+##  Text Splitter ##
+####################
